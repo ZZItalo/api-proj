4a38dd4814a91c3ab1ac33bdcf5c29978e8375e8
ITALIC <ffhfhn125@gmail.com>
2026-09-02 19:39:17 -0300

    feat: add a method to handle bad request
    - Add a method to handle HttpMessageNotReadableException at
    GlobalExceptionHandler

--- src/main/java/com/italo/task_api/advice/GlobalExceptionHandler.java
+++ src/main/java/com/italo/task_api/advice/GlobalExceptionHandler.java
@@ -4,6 +4,7 @@ import com.italo.task_api.dto.ErrorDetailsDto;
 import com.italo.task_api.exception.ResourceNotFoundException;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
+import org.springframework.http.converter.HttpMessageNotReadableException;
 import org.springframework.web.bind.annotation.ExceptionHandler;
 import org.springframework.web.bind.annotation.RestControllerAdvice;
 
@@ -19,5 +20,22 @@ public class GlobalExceptionHandler {
         errorDetails.setMessage(exception.getMessage());
 
         return ResponseEntity.status(HttpStatus.NOT_FOUND).body(errorDetails);
+    }
+
+    @ExceptionHandler(HttpMessageNotReadableException.class)
+    public ResponseEntity<ErrorDetailsDto> handleHttpMessageNotReadableException(
+            HttpMessageNotReadableException exception) {
+
+        ErrorDetailsDto errorDetailsDto = new ErrorDetailsDto();
+
+        String msg = "The API could not accept the request because its "
+                +"request body can not be read or converted to a "
+                +"Java object. Try checking for: Malformed JSON "
+                +"syntax (missing commas, unclosed brackets, etc), "
+                +"data type mismatches, invalid enum values, etc.";
+
+        errorDetailsDto.setMessage(msg);
+
+        return ResponseEntity.badRequest().body(errorDetailsDto);
     }
 }
